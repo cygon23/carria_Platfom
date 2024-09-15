f12fc0e31ecbc50fbff88dc5e4e7bfe972497282
Godfrey <rweyemamu088@gmail.com>
2024-09-15 12:26:12 +0300

    third commit 15th stay organized and removing unnesessary file

--- partials/sidebar.php
+++ partials/sidebar.php
@@ -1,16 +1,18 @@
+
 <aside class="sidebar">
         <button type="button" class="sidebar-close-btn">
             <iconify-icon icon="radix-icons:cross-2"></iconify-icon>
         </button>
         <div>
             <a href="index.php" class="sidebar-logo">
-               <img src="assets/images/" alt="">
+               <img src="assets/images/logo-cnm.png" alt="">
    
             </a>
         </div>
         <div class="sidebar-menu-area">
             <ul class="sidebar-menu" id="sidebar-menu">
                 <li class="dropdown">
+                    <!-- <a href="./auth/index2.php"> -->
                     <a href="index.php">
                         <iconify-icon icon="solar:home-smile-angle-outline" class="menu-icon"></iconify-icon>
                         <span>Dashboard</span>
@@ -19,7 +21,7 @@
                 </li>
                 <li class="sidebar-menu-group-title">Application</li>
                 <li>
-                    <a href="javascript:void(0)">
+                    <a href="./skills/skills.php">
                         <iconify-icon icon="mage:email" class="menu-icon"></iconify-icon>
                         <span>Skills</span>
                     </a>
@@ -34,18 +36,18 @@
                             <a href="javascript:void(0)"><i class="ri-circle-fill circle-icon text-primary-600 w-auto"></i> Create CV</a>
                         </li>
                         <li>
-                            <a href="invoice-preview.php"><i class="ri-circle-fill circle-icon text-warning-main w-auto"></i> Preview</a>
+                            <a href="./cv/invoice-preview.php"><i class="ri-circle-fill circle-icon text-warning-main w-auto"></i> Preview</a>
                         </li>
                         <li>
-                            <a href="invoice-add.php"><i class="ri-circle-fill circle-icon text-info-main w-auto"></i>Download CV</a>
+                            <a href="./cv/invoice-add.php"><i class="ri-circle-fill circle-icon text-info-main w-auto"></i>Download CV</a>
                         </li>
                         <li>
-                            <a href="invoice-edit.php"><i class="ri-circle-fill circle-icon text-danger-main w-auto"></i> Edit</a>
+                            <a href="./cv/invoice-edit.php"><i class="ri-circle-fill circle-icon text-danger-main w-auto"></i> Edit</a>
                         </li>
                     </ul>
                 </li>
                 <li>
-                    <a href="calendar-main.php">
+                    <a href="javascript:void(0)">
                         <iconify-icon icon="solar:calendar-outline" class="menu-icon"></iconify-icon>
                         <span>Training</span>
                     </a>
