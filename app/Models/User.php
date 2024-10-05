0f628562f80f1cc3a099c76c0ac7474019ed48f4
cygon23 <rweyemamu088@gmail.com>
2024-10-05 23:03:19 +0300

    modification in model to handle mass input

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,7 +20,11 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'image',
+        'designation',
+        'mobile',
     ];
+
 
     /**
      * The attributes that should be hidden for serialization.
