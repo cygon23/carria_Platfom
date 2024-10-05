66b903b122119ea989c18787e47944934bd74661
cygon23 <rweyemamu088@gmail.com>
2024-10-05 22:58:35 +0300

    authentification

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\homeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [homeController::class, 'index']);
+
+Route::get('/register', [AuthController::class, 'register']);
+Route::post('/register', [AuthController::class, 'processRegistration']);
+Route::get('/login', [AuthController::class, 'login']);
+Route::get('/login/auth', [AuthController::class, 'auth_login']);
