bdc1b837a7021a922b2b76c7f9a7a7cfc45779a5
cygon23 <rweyemamu088@gmail.com>
2024-10-06 09:47:41 +0300

    setting of middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,18 +1,29 @@
 <?php
 
+use Illuminate\Auth\Middleware\Authenticate;
+use Illuminate\Auth\Middleware\RedirectIfAuthenticated;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // Guest Middleware Group
+        $middleware->appendToGroup('guest', [
+            RedirectIfAuthenticated::class,
+        ]);
+
+        // Auth Middleware Group
+        $middleware->appendToGroup('auth', [
+            Authenticate::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+        // Exception handling logic can be added here
+    })
+    ->create();
