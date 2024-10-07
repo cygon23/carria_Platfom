c34f13b4f3e71cc6545d9879a88ff7f82d17ecc7
cygon23 <rweyemamu088@gmail.com>
2024-10-07 13:19:20 +0300

    DB seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use App\Models\Category;
+use App\Models\Job;
 use App\Models\JobType;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -16,8 +17,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Category::factory(5)->create();
-        JobType::factory(5)->create();
+        // Category::factory(5)->create();
+        // JobType::factory(5)->create();
+
+
+        Job::factory(25)->create();
 
         // User::factory()->create([
         //     'name' => 'Test User',
