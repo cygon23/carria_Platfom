ad070952a86cc1c74706c6108b2b9c5ab8a61832
cygon23 <rweyemamu088@gmail.com>
2024-10-07 13:17:44 +0300

    job list complete

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [AuthController::class, 'profile'])->name('profile');
     Route::post('/updateProfile', [AuthController::class, 'updateProfile'])->name('updateProfile');
     Route::post('/profileImage', [AuthController::class, 'profileImage'])->name('profileImage');
+    Route::get('/create-job', [AuthController::class, 'createJob'])->name('create-job');
+    Route::post('/save-job', [AuthController::class, 'saveJob'])->name('save-job');
+    Route::get('/my-jobs', [AuthController::class, 'myJobs'])->name('my-job');
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 });
