d1b258f5d4b1330e0227a2d3de92673ecc38737b
cygon23 <rweyemamu088@gmail.com>
2024-10-07 13:20:52 +0300

    Job model for job data handling

---
+++ app/Models/Job.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Job extends Model
+{
+    use HasFactory;
+
+    protected $table = 'jobs_';
+
+    protected $fillable = [
+        'title',
+        'category_id',
+        'job_type_id',
+        'vacancy',
+        'salary',
+        'location',
+        'description',
+        'benefits',
+        'responsibility',
+        'qualifications',
+        'keywords',
+        'experience',
+        'company_name',
+        'company_location',
+        'website',
+    ];
+
+    public function JobType()
+    {
+        return $this->belongsTo(JobType::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
