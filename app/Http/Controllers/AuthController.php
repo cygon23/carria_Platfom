36a4bf8e5fdee00c93c682729f2bd325bb77d928
cygon23 <rweyemamu088@gmail.com>
2024-10-07 06:46:59 +0300

    use GD in managing and picture resizing

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -130,8 +130,6 @@ class AuthController extends Controller
     }
 
 
-
-
     public function logout()
     {
         Auth::logout();
--- resources/views/auth/sidebar.blade.php
+++ resources/views/auth/sidebar.blade.php
@@ -1,7 +1,8 @@
 <div class="card border-0 shadow mb-4 p-3">
     <div class="s-body text-center mt-3">
         @if (Auth::user()->image != '')
-            <img src="{{ Auth::user()->image }}" alt="avatar" class="rounded-circle img-fluid" style="width: 150px;">
+            <img src="{{ Auth::user()->image }}" alt="avatar" class="rounded-circle img-fluid"
+                style="width: 150px; height: 150px; object-fit: cover;">
         @else
             <img src="{{ url('assets/images/avatar7.png') }}" alt="avatar" class="rounded-circle img-fluid"
                 style="width: 150px;">
