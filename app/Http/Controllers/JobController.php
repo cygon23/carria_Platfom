97c90a5b90384ee3fc94e9bc601233a2d9ce4001
cygon23 <rweyemamu088@gmail.com>
2024-10-08 13:04:52 +0300

    filtering completes

---
+++ app/Http/Controllers/JobController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Job;
+use App\Models\JobType;
+use Illuminate\Http\Request;
+
+class JobController extends Controller
+{
+    //showing the job page
+
+    public function index(Request $request)
+    {
+
+        $categories = Category::where('status', 1)->get();
+        $jobTypes = JobType::where('status', 1)->get();
+
+        $jobs = Job::where('status', 1);
+
+        //searching using keyword
+        if (!empty($request->keyword)) {
+            $jobs = $jobs->where(function ($e) use ($request) {
+                $e->orWhere('title', 'like', '%' . $request->keyword . '%');
+                $e->orWhere('keywords', 'like', '%' . $request->keyword . '%');
+            });
+        }
+
+        // seaching fusing location
+        if (!empty($request->location)) {
+            $jobs = $jobs->where('location', $request->location);
+        }
+
+        //searching using category
+
+        if (!empty($request->category)) {
+            $jobs = $jobs->where('category_id', $request->category);
+        }
+
+        //searching using job Type  user can join arous types such location,types etc
+
+        if (!empty($request->jobType)) {
+            $jobsTypeArray = explode(',', $request->jobType);
+            $jobs = $jobs->whereIn('job_type_id', $jobsTypeArray);
+        }
+
+
+
+
+        //searching using Exeprience
+
+        if (!empty($request->experience)) {
+            $jobs = $jobs->where('experience', $request->experience);
+        }
+
+        $jobs = $jobs->with(['JobType', 'category'])
+            ->orderBy('created_at', 'DESC')
+            ->paginate(9);
+
+
+
+        return view('front.jobs.jobs', [
+            'categories' =>   $categories,
+            'jobTypes' =>   $jobTypes,
+            'jobs' => $jobs
+        ]);
+    }
+}
