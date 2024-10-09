a6561fffd31309825bf34b72ee6233a11d1db739
cygon23 <rweyemamu088@gmail.com>
2024-10-09 07:25:52 +0300

    Apply job completes model

---
+++ app/Models/JobApplication.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class JobApplication extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['job_id', 'user_id', 'employer_id', 'applied_date'];
+}
