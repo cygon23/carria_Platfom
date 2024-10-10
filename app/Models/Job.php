f646957c6a8456676e3ed4488b4cf01c81d517a7
cygon23 <rweyemamu088@gmail.com>
2024-10-10 10:15:21 +0300

    applied applicant complete

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -39,4 +39,9 @@ class Job extends Model
     {
         return $this->belongsTo(Category::class);
     }
+
+    public function applications()
+    {
+        return $this->hasMany(JobApplication::class);
+    }
 }
