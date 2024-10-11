f066536c55325588139e2e319cf2ffda4ae66f2d
cygon23 <rweyemamu088@gmail.com>
2024-10-11 22:31:29 +0300

    changing passowrd feature is completes

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -67,23 +67,37 @@
 
                     <!-- Password Change Form -->
                     <div class="card border-0 shadow mb-4">
-                        <form action="" method="POST">
+
+                        <form action="{{ route('password-update') }}" method="POST">
+                            @csrf
                             <div class="card-body p-4">
                                 <h3 class="fs-4 mb-1">Change Password</h3>
                                 <div class="mb-4">
                                     <label for="old_password" class="mb-2">Old Password*</label>
                                     <input type="password" name="old_password" placeholder="Old Password"
                                         class="form-control">
+                                    @error('old_password')
+                                        <p class="m-0 small alert alert-danger shadow-sm">{{ $message }}</p>
+                                        <!-- Use $message for error messages -->
+                                    @enderror
                                 </div>
                                 <div class="mb-4">
                                     <label for="new_password" class="mb-2">New Password*</label>
                                     <input type="password" name="new_password" placeholder="New Password"
                                         class="form-control">
+                                    @error('new_password')
+                                        <p class="m-0 small alert alert-danger shadow-sm">{{ $message }}</p>
+                                        <!-- Use $message for error messages -->
+                                    @enderror
                                 </div>
                                 <div class="mb-4">
                                     <label for="confirm_password" class="mb-2">Confirm Password*</label>
                                     <input type="password" name="confirm_password" placeholder="Confirm Password"
                                         class="form-control">
+                                    @error('confirm_password')
+                                        <p class="m-0 small alert alert-danger shadow-sm">{{ $message }}</p>
+                                        <!-- Use $message for error messages -->
+                                    @enderror
                                 </div>
                             </div>
                             <div class="card-footer p-4">
