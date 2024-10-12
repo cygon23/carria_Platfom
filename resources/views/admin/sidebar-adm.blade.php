c3a6d32ae31289779fe5f7394736ca000f9e190e
cygon23 <rweyemamu088@gmail.com>
2024-10-12 12:16:30 +0300

    setting up Admin dashboard with middleware

---
+++ resources/views/admin/sidebar-adm.blade.php
@@ -0,0 +1,34 @@
+<div class="card border-0 shadow mb-4 p-3">
+    <div class="s-body text-center mt-3">
+        @if (Auth::user()->image != '')
+            <img src="{{ Auth::user()->image }}" alt="avatar" class="rounded-circle img-fluid"
+                style="width: 150px; height: 150px; object-fit: cover;">
+        @else
+            <img src="{{ url('assets/images/avatar7.png') }}" alt="avatar" class="rounded-circle img-fluid"
+                style="width: 150px;">
+        @endif
+
+        <h5 class="mt-3 pb-0">{{ Auth::user()->name }}</h5>
+        <div class="d-flex justify-content-center mb-2">
+
+        </div>
+    </div>
+</div>
+<div class="card account-nav border-0 shadow mb-4 mb-lg-0">
+    <div class="card-body p-0">
+        <ul class="list-group list-group-flush ">
+            <li class="list-group-item d-flex justify-content-between p-3">
+                <a href="#">Users</a>
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center p-3">
+                <a href="#">Jobs</a>
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center p-3">
+                <a href="#">Jobs Application</a>
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center p-3">
+                <a href="{{ route('logout') }}">logout</a>
+            </li>
+        </ul>
+    </div>
+</div>
