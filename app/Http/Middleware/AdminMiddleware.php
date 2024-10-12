8ee0e34949ebb418cbfff8287a217ec20a1d9fcf
cygon23 <rweyemamu088@gmail.com>
2024-10-12 12:17:25 +0300

    setting up Admin dashboard with middleware

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // Check if the user is authenticated and has the 'admin' role
+        if (Auth::check() && Auth::user()->role === 'admin') {
+            return $next($request);
+        } else {
+            abort(404);
+        }
+
+        // Return a forbidden response if the user is not an admin
+
+    }
+}
