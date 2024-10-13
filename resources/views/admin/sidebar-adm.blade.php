ae8d95a054ecefdaa94aea82135ce8575fe1889c
cygon23 <rweyemamu088@gmail.com>
2024-10-13 14:08:19 +0300

    edting user complete

--- resources/views/admin/sidebar-adm.blade.php
+++ resources/views/admin/sidebar-adm.blade.php
@@ -18,7 +18,7 @@
     <div class="card-body p-0">
         <ul class="list-group list-group-flush ">
             <li class="list-group-item d-flex justify-content-between p-3">
-                <a href="#">Users</a>
+                <a href="{{ route('dashboard.users') }}">Users List</a>
             </li>
             <li class="list-group-item d-flex justify-content-between align-items-center p-3">
                 <a href="#">Jobs</a>
