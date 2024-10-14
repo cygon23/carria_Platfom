867303d77660568afade021b2c9ecd302efbb485
cygon23 <rweyemamu088@gmail.com>
2024-10-14 10:00:36 +0300

    Admin delete job Application completes

--- app/Http/Controllers/admin/JobAdminController.php
+++ app/Http/Controllers/admin/JobAdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use App\Models\Job;
+use App\Models\JobApplication;
 use App\Models\JobType;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
