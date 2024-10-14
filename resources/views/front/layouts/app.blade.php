fe0a9df364ade15949c1a70aea21de2d7bc5b9ed
cygon23 <rweyemamu088@gmail.com>
2024-10-14 21:51:28 +0300

    Normalize line endings

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -24,6 +24,26 @@
 
 
 
+
+    <link href="{{ url('test/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ url('test/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ url('test/assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+    <link href="{{ url('test/assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
+    <link href="{{ url('test/assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
+    <link href="{{ url('test/assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
+    <link href="{{ url('test/assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.gstatic.com" rel="preconnect">
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="{{ url('test/assets/css/style.css') }}" rel="stylesheet">
+
+
+
 </head>
 
 <body data-instant-intensity="mousedown">
@@ -106,6 +126,26 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.27.3/trumbowyg.min.js"
         integrity="sha512-YJgZG+6o3xSc0k5wv774GS+W1gx0vuSI/kr0E0UylL/Qg/noNspPtYwHPN9q6n59CTR/uhgXfjDXLTRI+uIryg=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+
+    <!----for try folder-->
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="{{ url('test/assets/vendor/apexcharts/apexcharts.min.js') }}"></script>
+    <script src="{{ url('test/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ url('test/assets/vendor/chart.js/chart.umd.js') }}"></script>
+    <script src="{{ url('test/assets/vendor/echarts/echarts.min.js') }}"></script>
+    <script src="{{ url('test/assets/vendor/quill/quill.js') }}"></script>
+    <script src="{{ url('test/assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
+    <script src="{{ url('test/assets/vendor/tinymce/tinymce.min.js') }}"></script>
+    <script src="{{ url('test/assets/vendor/php-email-form/validate.js') }}"></script>
+
+    <!-- Template Main JS File -->
+    <script src="{{ url('test/assets/js/main.js') }}"></script>
+
 
     <script>
         $('.textarea').trumbowyg();
