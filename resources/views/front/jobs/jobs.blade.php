e8c0ca5f2587fc4466f2e02d4eb539acc32f8f3f
cygon23 <rweyemamu088@gmail.com>
2024-10-14 10:37:25 +0300

    job details fixing

--- resources/views/front/jobs/jobs.blade.php
+++ resources/views/front/jobs/jobs.blade.php
@@ -135,6 +135,9 @@
                                             </div>
                                         </div>
                                     @endforeach
+                                    <div class="col-md-12">
+                                        {{ $jobs->withQueryString()->links() }}
+                                    </div>
                                 @else
                                     <div class="col-md-12">Jobs Not found</div>
                                 @endif
