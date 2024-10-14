4ba3b78204f06c504bb9e1e146be14e2600b8a7b
cygon23 <rweyemamu088@gmail.com>
2024-10-14 12:05:23 +0300

    add read more feature

--- resources/views/front/jobs/edit.blade.php
+++ resources/views/front/jobs/edit.blade.php
@@ -9,7 +9,7 @@
                 <div class="col">
                     <nav aria-label="breadcrumb" class=" rounded-3 p-3 mb-4">
                         <ol class="breadcrumb mb-0">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item"><a href="{{ route('/') }}">Home</a></li>
                             <li class="breadcrumb-item active">Post a Job</li>
                         </ol>
                     </nav>
