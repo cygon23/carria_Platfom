1e3efd06432cbbbd6a41f26a6706c736a32d806f
cygon23 <rweyemamu088@gmail.com>
2024-10-15 09:04:04 +0300

    Token exeperation feature added

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -133,20 +133,6 @@
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
             class="bi bi-arrow-up-short"></i></a>
 
-    <!-- Vendor JS Files -->
-    <script src="{{ url('test/assets/vendor/apexcharts/apexcharts.min.js') }}"></script>
-    <script src="{{ url('test/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ url('test/assets/vendor/chart.js/chart.umd.js') }}"></script>
-    <script src="{{ url('test/assets/vendor/echarts/echarts.min.js') }}"></script>
-    <script src="{{ url('test/assets/vendor/quill/quill.js') }}"></script>
-    <script src="{{ url('test/assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
-    <script src="{{ url('test/assets/vendor/tinymce/tinymce.min.js') }}"></script>
-    <script src="{{ url('test/assets/vendor/php-email-form/validate.js') }}"></script>
-
-    <!-- Template Main JS File -->
-    <script src="{{ url('test/assets/js/main.js') }}"></script>
-
-
     <script>
         $('.textarea').trumbowyg();
     </script>
