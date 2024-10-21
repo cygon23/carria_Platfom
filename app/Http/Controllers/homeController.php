1fa91fa66877a96782f4c0bb78bad1d8dea43eda
cygon23 <rweyemamu088@gmail.com>
2024-10-21 17:48:19 +0300

    home search feature added

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -13,6 +13,7 @@ class homeController extends Controller
     {
 
         $categories = Category::where('status', 1)->orderBY('name', 'ASC')->take(8)->get();
+        $newCategories = Category::where('status', 1)->orderBY('name', 'ASC')->get();
         $featuredJobs = Job::where('status', 1)
             ->orderBY('created_at', 'DESC')
             ->with('JobType')
@@ -30,7 +31,51 @@ class homeController extends Controller
             'categories' =>  $categories,
             'featuredJobs' =>  $featuredJobs,
             'latestJobs' =>   $latestJobs,
+            'newCategories' => $newCategories
 
         ]);
     }
+
+
+
+    // public function index()
+    // {
+    //     // Fetching categories with a count of active jobs (status = 1)
+    //     $categories = Category::where('status', 1) // Only active categories
+    //         ->withCount(['jobs' => function ($query) {
+    //             $query->where('status', 1); // Count only active jobs
+    //         }])
+    //         ->orderBy('name', 'ASC')
+    //         ->take(8)
+    //         ->get();
+
+    //     $newCategories = Category::where('status', 1)
+    //         ->withCount(['jobs' => function ($query) {
+    //             $query->where('status', 1); // Count only active jobs
+    //         }])
+    //         ->orderBy('name', 'ASC')
+    //         ->get();
+
+    //     // Featured and latest jobs
+    //     $featuredJobs = Job::where('status', 1)
+    //         ->orderBy('created_at', 'DESC')
+    //         ->with('JobType')
+    //         ->where('isFeatured', 1)
+    //         ->take(6)
+    //         ->get();
+
+    //     $latestJobs = Job::where('status', 1)
+    //         ->with('JobType')
+    //         ->orderBy('created_at', 'DESC')
+    //         ->take(6)
+    //         ->get();
+
+    //     // Passing data to the view
+    //     return view('front.home', [
+    //         'categories' => $categories,
+    //         'featuredJobs' => $featuredJobs,
+    //         'latestJobs' => $latestJobs,
+    //         'newCategories' => $newCategories
+    //     ]);
+    // }
 }
