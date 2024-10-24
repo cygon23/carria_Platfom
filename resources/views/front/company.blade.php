85f760314be0060adde1d8bf34117b622b3a71de
cygon23 <rweyemamu088@gmail.com>
2024-10-24 21:44:34 +0300

    company  view

--- resources/views/front/company.blade.php
+++ resources/views/front/company.blade.php
@@ -1,84 +1,3 @@
-{{-- @extends('front.layouts.app')
-
-@section('main')
-    <div class="container my-5">
-        <h2 class="text-center mb-4">Featured Companies</h2>
-
-        <div class="row">
-            <!-- First Company Card -->
-            <div class="col-md-4 mb-5">
-                <div class="card shadow-sm">
-                    <img src="{{ url('assets/images/jobs/driving.jpg') }}" alt="Company 1" class="card-img-top"
-                        style="height: 200px; object-fit: cover;">
-                    <div class="card-body">
-                        <h5 class="card-title">Private Driver</h5>
-                        <p class="card-text">Summary: Headquartered in Dar Es Salaam, Tanzania, DUTENI
-                            is a vibrant growing Company providing premier solutions to our corporate clients and unique
-                            experiences to our tourism clients..</p>
-
-                        <ul class="list-unstyled">
-                            <li><i class="fas fa-briefcase"></i> Available Jobs: 15</li>
-                            <li><i class="fas fa-map-marker-alt"></i> Location: Dar es salaam, Tz</li>
-                        </ul>
-
-                        <a href="#" class="btn btn-primary">
-                            <i class="fas fa-info-circle"></i> View Details
-                        </a>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Second Company Card -->
-            <div class="col-md-4 mb-5">
-                <div class="card shadow-sm">
-                    <img src="{{ url('assets/images/jobs/brodcast.jpg') }}" alt="Company 2" class="card-img-top"
-                        style="height: 200px; object-fit: cover;">
-                    <div class="card-body">
-                        <h5 class="card-title">OMNI-SOLUTIONS</h5>
-                        <p class="card-text">We are seeking for a motivated and experienced Customer Support Manager to lead
-                            our customer support team and ensure that we provide outstanding support to our customers. As a
-                            Customer Support Manager, you will be responsible for overseeing the customer support
-                            department,
-                            managing a team of support agents, and ensuring that our customers receive timely, effective,
-                            and courteous assistance.</p>
-
-                        <ul class="list-unstyled">
-                            <li><i class="fas fa-briefcase"></i> Available Jobs: 8</li>
-                            <li><i class="fas fa-map-marker-alt"></i> Location:Dar es Salaam , Tz</li>
-                        </ul>
-
-                        <a href="#" class="btn btn-primary">
-                            <i class="fas fa-info-circle"></i> View Details
-                        </a>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Third Company Card -->
-            <div class="col-md-4 mb-5">
-                <div class="card shadow-sm">
-                    <img src="{{ url('assets/images/jobs/network.jpg') }}" alt="Company 3" class="card-img-top"
-                        style="height: 200px; object-fit: cover;">
-                    <div class="card-body">
-                        <h5 class="card-title">JA Africa</h5>
-                        <p class="card-text">A leading firm in sustainability and green technology, Future Enterprises is
-                            dedicated to eco-friendly solutions.</p>
-
-                        <ul class="list-unstyled">
-                            <li><i class="fas fa-briefcase"></i> Available Jobs: 12</li>
-                            <li><i class="fas fa-map-marker-alt"></i> Location: Arusha, Tz</li>
-                        </ul>
-
-                        <a href="#" class="btn btn-primary">
-                            <i class="fas fa-info-circle"></i> View Details
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-@endsection --}}
-
 @extends('front.layouts.app')
 
 
@@ -139,7 +58,7 @@
                                 <p class="card-text">
                                     <strong>Location:</strong> Arusha, TZ
                                 </p>
-                                <a href="#" class="btn btn-primary">
+                                <a href="{{ route('company.ja-africa') }}" class="btn btn-primary">
                                     <i class="fas fa-info-circle"></i> Learn More
                                 </a>
                             </div>
