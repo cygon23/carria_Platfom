0c1a3d352627b67a067fda556ff2b2fc4e454a0a
cygon23 <rweyemamu088@gmail.com>
2024-10-26 13:30:57 +0300

    user create their own cv feature complete

--- app/Models/Experience.php
+++ app/Models/Experience.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Experience extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'basic_id',
+        'job_title',
+        'company_name',
+        'location',
+        'start_date',
+        'end_date',
+        'description',
+    ];
+
+    // Relationship with the Basic model
+    public function basic()
+    {
+        return $this->belongsTo(CvBasicInfo::class);
+    }
 }
