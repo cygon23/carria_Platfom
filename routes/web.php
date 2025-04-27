7552ed495b2a1ba71053388652cabb0e7ac421d5
cygon23 <rweyemamu088@gmail.com>
2025-04-27 17:58:12 +0300

    addting of laravel horizon for overseeing running process and all actions

--- routes/web.php
+++ routes/web.php
@@ -53,7 +53,6 @@ Route::middleware('guest')->group(function () {
     Route::get('/login', [AuthController::class, 'login'])->name('login');
     Route::post('/login/authenticate', [AuthController::class, 'authenticate'])->name('login.authenticate');
     Route::get('verify/{token}', [AuthController::class, 'verify']);
-
     //email verification
     Route::get('/email/verify', [AuthController::class, 'verifyemail'])->name('verification.notice');
 });
